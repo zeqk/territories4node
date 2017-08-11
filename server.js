fdab94a4770341a385b1d972fd7e772dc608e859
eperez <perez.e@softlab.umet.edu.ar>
2017-08-11 13:28:59 -0300

    cambios

--- server.js
+++ server.js
@@ -1,7 +1,13 @@
 var express = require('express'),
   app = express(),  
   // Territory = require('./api/models/territoryModel'),
-  bodyParser = require('body-parser');
+  bodyParser = require('body-parser'),
+  morgan  = require('morgan');
+    
+Object.assign=require('object-assign')
+
+app.engine('html', require('ejs').renderFile);
+app.use(morgan('combined'));
 
 var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
   ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0',
@@ -52,16 +58,56 @@ var initDb = function(callback) {
 };
 
 
+app.get('/', function (req, res) {
+  res.send('Hello from Territories  at '+ new Date());
+  // try to initialize the db on every request if it's not already
+  // initialized.
+  if (!db) {
+    initDb(function(err){});
+  }
+  if (db) {
+    var col = db.collection('counts');
+    // Create a document with request IP and current time of request
+    col.insert({ip: req.ip, date: Date.now()});
+    col.count(function(err, count){
+      res.render('index.html', { pageCountMessage : count, dbInfo: dbDetails });
+    });
+  } else {
+    res.render('index.html', { pageCountMessage : null});
+  }
+});
+
+app.get('/pagecount', function (req, res) {
+  // try to initialize the db on every request if it's not already
+  // initialized.
+  if (!db) {
+    initDb(function(err){});
+  }
+  if (db) {
+    db.collection('counts').count(function(err, count ){
+      res.send('{ pageCount: ' + count + '}');
+    });
+  } else {
+    res.send('{ pageCount: -1 }');
+  }
+});
+
+// error handling
+app.use(function(err, req, res, next){
+  console.error(err.stack);
+  res.status(500).send('Something bad happened!');
+});
+
+initDb(function(err){
+  console.log('Error connecting to Mongo. Message:\n'+err);
+});
+
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 
 var routes = require('./api/routes/territoriesRoutes');
 routes(app);
-
-app.get('/', function(req, res) {
-    res.send('Hello from Territories  at '+ new Date());
-});
 
 
 app.listen(port,ip);
